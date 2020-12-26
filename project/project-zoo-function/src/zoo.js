972c52f6b80a356f0eaca3a1533c51b881789556
albertassi88 <albertassi88@gmail.com>
2020-12-26 10:52:23 -0300

    Alteracoes no projeto zoo-function

--- project/project-zoo-function/src/zoo.js
+++ project/project-zoo-function/src/zoo.js
@@ -17,7 +17,6 @@ Ao receber mais de um id, retorna um array com as espécies referentes aos ids*/
 function animalsByIds(...ids) {
     return data.animals.filter(animal => ids.includes(animal.id));
 }
-console.log(animalsByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
 
 
 /*2. IMPLEMENTE A FUNÇÃO animalsOlderThan
